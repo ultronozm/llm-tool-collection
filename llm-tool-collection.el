ffa3d268f004708d3900849b066c96490ec77e91
Ad <me@skissue.xyz>
2025-01-19 13:12:48 -0500

    feat: package scaffolding

---
+++ llm-tool-collection.el
@@ -0,0 +1,37 @@
+;;; llm-tool-collection.el --- Crowdsourced tools for LLMs -*- lexical-binding: t -*-
+
+;; Author: Ad
+;; Maintainer: Ad
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
+;; Homepage: https://github.com/skissue/llm-tool-collection
+;; Keywords: tools, convenience
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Crowdsourced collection of tools for LLMs in Emacs
+
+;;; Code:
+
+
+
+(provide 'llm-tool-collection)
+
+;;; llm-tool-collection.el ends here
