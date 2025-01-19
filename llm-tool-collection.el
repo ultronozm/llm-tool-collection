5d8c27141f4fb94b3f70ac3df06062bbefa48814
Ad <me@skissue.xyz>
2025-01-19 13:24:58 -0500

    feat: macro to define tools

--- llm-tool-collection.el
+++ llm-tool-collection.el
@@ -3,7 +3,7 @@
 ;; Author: Ad
 ;; Maintainer: Ad
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.1"))
+;; Package-Requires: ((emacs "28.1"))
 ;; Homepage: https://github.com/skissue/llm-tool-collection
 ;; Keywords: tools, convenience
 
@@ -30,7 +30,28 @@
 
 ;;; Code:
 
+(defmacro llm-tool-collection-deftool (name &rest specs)
+  "Declare a generic LLM tool named NAME.
+The remaining arguments SPECS should be a plist specifying the standard
+attributes of an LLM tool."
+  (declare (indent defun))
+  `(defconst ,(intern (concat "llm-tool-collection-tool-"
+                              (string-replace "_" "-" name)))
+     (:name ,name
+            ,@specs)))
 
+(llm-tool-collection-deftool "read_file"
+  :description "Read the contents of a file"
+  :args (list '(:name "path"
+                      :type "string"
+                      :description "Path to the file to read. Supports relative paths and ~."))
+  :category "filesystem"
+  :include t
+  :confirm t
+  :function (lambda (path)
+              (with-temp-buffer
+                (insert-file-contents (expand-file-name path))
+                (buffer-string))))
 
 (provide 'llm-tool-collection)
 
